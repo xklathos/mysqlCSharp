a7a69081936d0747ab50e05e40080d77c219e3c5
xklathos <xklathos@gmail.com>
2013-12-17 10:20:50 +0200

    first

---
+++ mysqlCSharp/dbConnector.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace mysqlCSharp
+{
+    partial class dbConnector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.srvName = new System.Windows.Forms.TextBox();
+            this.usrName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dbName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.getTbls = new System.Windows.Forms.Button();
+            this.listTable = new System.Windows.Forms.ListBox();
+            this.getData = new System.Windows.Forms.Button();
+            this.personOidList = new System.Windows.Forms.ListBox();
+            this.selectUsers = new System.Windows.Forms.Button();
+            this.usersSelected = new System.Windows.Forms.ListBox();
+            this.testButton = new System.Windows.Forms.Button();
+            this.logger = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(83, 162);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(70, 23);
+            this.submit.TabIndex = 1;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // srvName
+            // 
+            this.srvName.Location = new System.Drawing.Point(83, 16);
+            this.srvName.Name = "srvName";
+            this.srvName.Size = new System.Drawing.Size(95, 20);
+            this.srvName.TabIndex = 2;
+            this.srvName.Text = "localhost";
+            // 
+            // usrName
+            // 
+            this.usrName.Location = new System.Drawing.Point(83, 42);
+            this.usrName.Name = "usrName";
+            this.usrName.Size = new System.Drawing.Size(95, 20);
+            this.usrName.TabIndex = 4;
+            this.usrName.Text = "root";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "user";
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(83, 68);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(95, 20);
+            this.pass.TabIndex = 6;
+            this.pass.Text = "root";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "pass";
+            // 
+            // dbName
+            // 
+            this.dbName.Location = new System.Drawing.Point(83, 94);
+            this.dbName.Name = "dbName";
+            this.dbName.Size = new System.Drawing.Size(95, 20);
+            this.dbName.TabIndex = 8;
+            this.dbName.Text = "mit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "dbName";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "port";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(83, 124);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(95, 20);
+            this.portBox.TabIndex = 10;
+            this.portBox.Text = "3306";
+            // 
+            // getTbls
+            // 
+            this.getTbls.Enabled = false;
+            this.getTbls.Location = new System.Drawing.Point(246, 12);
+            this.getTbls.Name = "getTbls";
+            this.getTbls.Size = new System.Drawing.Size(75, 23);
+            this.getTbls.TabIndex = 11;
+            this.getTbls.Text = "Get Tables";
+            this.getTbls.UseVisualStyleBackColor = true;
+            this.getTbls.Click += new System.EventHandler(this.getTbls_Click);
+            // 
+            // listTable
+            // 
+            this.listTable.FormattingEnabled = true;
+            this.listTable.Location = new System.Drawing.Point(227, 41);
+            this.listTable.Name = "listTable";
+            this.listTable.Size = new System.Drawing.Size(120, 212);
+            this.listTable.TabIndex = 12;
+            // 
+            // getData
+            // 
+            this.getData.Enabled = false;
+            this.getData.Location = new System.Drawing.Point(371, 12);
+            this.getData.Name = "getData";
+            this.getData.Size = new System.Drawing.Size(75, 23);
+            this.getData.TabIndex = 13;
+            this.getData.Text = "Get Data";
+            this.getData.UseVisualStyleBackColor = true;
+            this.getData.Click += new System.EventHandler(this.getData_Click);
+            // 
+            // personOidList
+            // 
+            this.personOidList.FormattingEnabled = true;
+            this.personOidList.Location = new System.Drawing.Point(353, 41);
+            this.personOidList.MultiColumn = true;
+            this.personOidList.Name = "personOidList";
+            this.personOidList.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.personOidList.Size = new System.Drawing.Size(120, 212);
+            this.personOidList.TabIndex = 14;
+            // 
+            // selectUsers
+            // 
+            this.selectUsers.Enabled = false;
+            this.selectUsers.Location = new System.Drawing.Point(497, 12);
+            this.selectUsers.Name = "selectUsers";
+            this.selectUsers.Size = new System.Drawing.Size(75, 23);
+            this.selectUsers.TabIndex = 15;
+            this.selectUsers.Text = "Select Users";
+            this.selectUsers.UseVisualStyleBackColor = true;
+            this.selectUsers.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // usersSelected
+            // 
+            this.usersSelected.Enabled = false;
+            this.usersSelected.FormattingEnabled = true;
+            this.usersSelected.Location = new System.Drawing.Point(479, 41);
+            this.usersSelected.Name = "usersSelected";
+            this.usersSelected.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.usersSelected.Size = new System.Drawing.Size(120, 212);
+            this.usersSelected.TabIndex = 16;
+            // 
+            // testButton
+            // 
+            this.testButton.Enabled = false;
+            this.testButton.Location = new System.Drawing.Point(353, 278);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(120, 23);
+            this.testButton.TabIndex = 17;
+            this.testButton.Text = "Create Table";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // logger
+            // 
+            this.logger.FormattingEnabled = true;
+            this.logger.Location = new System.Drawing.Point(12, 307);
+            this.logger.Name = "logger";
+            this.logger.Size = new System.Drawing.Size(587, 108);
+            this.logger.TabIndex = 18;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.usrName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.submit);
+            this.panel1.Controls.Add(this.srvName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pass);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dbName);
+            this.panel1.Controls.Add(this.portBox);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 197);
+            this.panel1.TabIndex = 19;
+            // 
+            // dbConnector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(618, 434);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.logger);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.usersSelected);
+            this.Controls.Add(this.selectUsers);
+            this.Controls.Add(this.personOidList);
+            this.Controls.Add(this.getData);
+            this.Controls.Add(this.listTable);
+            this.Controls.Add(this.getTbls);
+            this.Name = "dbConnector";
+            this.Text = "DbConnecter";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.TextBox srvName;
+        private System.Windows.Forms.TextBox usrName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dbName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Button getTbls;
+        private System.Windows.Forms.ListBox listTable;
+        private System.Windows.Forms.Button getData;
+        private System.Windows.Forms.ListBox personOidList;
+        private System.Windows.Forms.Button selectUsers;
+        private System.Windows.Forms.ListBox usersSelected;
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.ListBox logger;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ mysqlCSharp/dbConnector.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections;
+//test
+
+namespace mysqlCSharp
+{
+    public partial class dbConnector : Form
+    {
+        ArrayList selectedUsers = new ArrayList();
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string pwd;
+        public string tableName;
+        public string person_oid;
+
+        public dbConnector()
+        {
+            InitializeComponent();
+
+        }
+        public void Initialize()
+        {
+            server = srvName.Text;
+            database = dbName.Text;
+            uid = usrName.Text;
+            pwd = pass.Text;
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + pwd + ";";
+            logger.Items.Add(connectionString);
+            connection = new MySqlConnection(connectionString);
+
+        }
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Database Connected !");
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public void listTables()
+        {
+            MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = "show tables";
+            logger.Items.Add(cmd.CommandText);
+            try
+            {
+                Initialize();
+                connection.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string row = "";
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        row += reader.GetValue(i).ToString();
+
+                    listTable.Items.Add(row);
+                }
+                reader.Close();
+
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Number.ToString());
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void submit_Click(object sender, EventArgs e)
+        {
+            Initialize();
+            OpenConnection();
+            getTbls.Enabled = true;
+        }
+        private void getData_Click(object sender, EventArgs e)
+        {
+
+            usersSelected.Enabled = true;
+            selectUsers.Enabled = true;
+            string tableName = listTable.Text;
+            MySqlCommand command = connection.CreateCommand();
+            string sql = "SELECT person_oid FROM " + tableName + " GROUP BY person_oid";
+            command.CommandText = sql;
+            logger.Items.Add(command.CommandText);
+            MySqlDataReader Reader = command.ExecuteReader();
+
+            while (Reader.Read())
+            {
+                string thisrow = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                    thisrow += Reader.GetValue(i).ToString();
+
+                personOidList.Items.Add(thisrow);
+
+            }
+            Reader.Close();
+        }
+        private void getTbls_Click(object sender, EventArgs e)
+        {
+            listTables();
+            personOidList.Enabled = true;
+            getData.Enabled = true;
+
+
+        }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            testButton.Enabled = true;
+            String strItem;
+            foreach (Object selecteditem in personOidList.SelectedItems)
+            {
+
+                strItem = selecteditem as String;
+                usersSelected.Items.Add(strItem);
+                selectedUsers.Add(strItem);
+
+            }
+        }
+
+
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+
+            for (int j = 0; j < usersSelected.Items.Count; j++)
+            {
+                string item = usersSelected.Items[j].ToString();
+                string tableName = listTable.Text;
+                MySqlCommand command = connection.CreateCommand();
+                string testTable = "CREATE TABLE " + tableName + item + " LIKE " + tableName + "; INSERT INTO " + tableName + item + " SELECT * FROM " + tableName + " WHERE person_oid=" + item + " ORDER BY starttime;";
+                command.CommandText = testTable;
+                logger.Items.Add(command.CommandText);
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+    }
+}
+
+
+
+
